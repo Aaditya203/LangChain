658e10fcc6b2bcddbe93ed5536b00d4579fe6a12
Aditya Sharma <adityas20032005@gmail.com>
2026-05-01 11:37:11 +0530

    using annotated and optional

--- StructuredOutput/with_structured.py
+++ StructuredOutput/with_structured.py
@@ -1,17 +1,19 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-from typing import TypedDict
+from typing import TypedDict,Annotated,Optional
 
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 
 class Review(TypedDict):
-    summary:str
-    sentiment:str
+    summary:Annotated[str,"A brief summary of the review"]
+    sentiment:Annotated[str,"from the review give its sentiment as positive,negative,neutral"]
+    name:Annotated[Optional[str],"Write the name of the reviewer"]
 
 structured_model = model.with_structured_output(Review)
 
-result = structured_model.invoke("""The hardware is great, but the software feels bloated. There are too many pre-installed apps that I can't remove. Also, the UI looks outdated compared to other brands. Hoping for a software update to fix this.""")
+result = structured_model.invoke("""The hardware is great, but the software feels bloated. There are too many pre-installed apps that I can't remove. Also, the UI looks outdated compared to other brands. Hoping for a software update to fix this.
+                                 By Aditya Sharma""")
 
 print(result)
